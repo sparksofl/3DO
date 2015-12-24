adfef88949995fb627ca072d0e284d8e5fa68f94
Mary Solodovnik <gimiro42@gmail.com>
2015-12-24 15:45:52 +0200

    rake db:seed:dump 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,5 +16,6 @@ User.create!([
   {username: "John", email: "john@doe", password_digest: "$2a$10$7F8n6P6HEH4JAatPf/PJz.QXH39LKfy7KT.okCqJpe3dSG5J7nddi", model_id: nil},
   {username: "Alise", email: "alise@com.ua", password_digest: "$2a$10$IVqnSwxpef33yRPBAP3UOunZIg7Ml06UVe9uOn.0vjpNiN8DI2XoG", model_id: nil},
   {username: "mary", email: "mary@com", password_digest: "$2a$10$eW5/Mt9/bGXkVmrbemQYV.JjSfgQDnnrfbKR35bfEXZO4UYgT.3vm", model_id: nil},
-  {username: "gale", email: "gale@com", password_digest: "$2a$10$EeMIFeL3WQiKgx0rtH/BNePxSIXF1MW0y53r2.xhERxTuD9fboQs6", model_id: nil}
+  {username: "gale", email: "gale@com", password_digest: "$2a$10$EeMIFeL3WQiKgx0rtH/BNePxSIXF1MW0y53r2.xhERxTuD9fboQs6", model_id: nil},
+  {username: "jane", email: "jane@doe", password_digest: "$2a$10$OsdXyu5VoG/PQtEQE/XoYuGx5pOO5u/Rontr0oT1jAWuSL71vjbIO", model_id: nil}
 ])
